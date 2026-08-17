a1fe61b43de9adae2706944c4270cbf3b732bd1a
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-17 16:21:47 +0200

    fix: fix env.py import

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,6 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from sqlalchemy.orm import DeclarativeBase
 
 import city.models
 #import temperature.models
