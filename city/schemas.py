2c470a996219a3a3029b99670a22f1fe3d4751ef
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-18 19:14:46 +0200

    feat: add CityUpdate schema

--- city/schemas.py
+++ city/schemas.py
@@ -12,6 +12,11 @@ class CityCreate(CityBase):
     pass
 
 
+class CityUpdate(CityBase):
+    name: Annotated[str, Field(max_length=100)] | None = None
+    additional_info: str | None = None
+
+
 class CityRead(CityBase):
     id: int
     model_config = ConfigDict(from_attributes=True)
