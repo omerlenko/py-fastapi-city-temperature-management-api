218e45c0a1178b602150afbb7a646dad65986fbf
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-18 19:21:31 +0200

    feat: add read_city, create_city, update_city, delete_city endpoints

--- city/router.py
+++ city/router.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from city import schemas, crud
@@ -14,3 +14,28 @@ DbDep = Annotated[AsyncSession, Depends(get_db)]
 @router.get("/cities/", response_model=list[schemas.CityRead])
 async def read_cities(db: DbDep, skip: int = 0, limit: int = 10):
     return await crud.get_cities(db=db, skip=skip, limit=limit)
+
+@router.get("/cities/{city_id}", response_model=schemas.CityRead)
+async def read_city(db: DbDep, city_id: int):
+    city = crud.get_city(db=db, city_id=city_id)
+    if city is None:
+        raise HTTPException(status_code=404, detail="This city does not exist in the database")
+    return city
+
+@router.post("/cities/", response_model=schemas.CityRead, status_code=201)
+async def create_city(db: DbDep, data: schemas.CityCreate):
+    return await crud.create_city(db=db, data=data)
+
+@router.patch("/cities/{city_id}/", response_model=schemas.CityRead)
+async def update_city(db: DbDep, city_id: int, data: schemas.CityUpdate):
+    city = await crud.get_city(db=db, city_id=city_id)
+    if city is None:
+        raise HTTPException(status_code=404, detail="This city does not exist in the database")
+    return await crud.update_city(db=db, city=city, data=data)
+
+@router.delete("/cities/{city_id}", status_code=204)
+async def delete_city(db: DbDep, city_id: int):
+    city = await crud.get_city(db=db, city_id=city_id)
+    if city is None:
+        raise HTTPException(status_code=404)
+    await crud.delete_city(db=db, city=city)
