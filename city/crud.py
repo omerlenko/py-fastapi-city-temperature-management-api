47a5c1462a08cfe4d719529848c19a91dd3c6062
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-19 12:51:34 +0200

    feat: handle city name unique constraint error

--- city/crud.py
+++ city/crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from city import schemas, models
@@ -13,16 +14,24 @@ async def get_city(db: AsyncSession, city_id: int) -> models.City | None:
     return await db.get(models.City, city_id)
 
 async def create_city(db: AsyncSession, data: schemas.CityCreate) -> models.City:
-    city = models.City(data.model_dump())
+    city = models.City(**data.model_dump())
     db.add(city)
-    await db.commit()
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise
     await db.refresh(city)
     return city
 
 async def update_city(db: AsyncSession, city: models.City, data: schemas.CityUpdate) -> models.City:
     for key, value in data.model_dump(exclude_unset=True).items():
         setattr(city, key, value)
-    await db.commit()
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise
     return city
 
 async def delete_city(db: AsyncSession, city: models.City) -> None:
