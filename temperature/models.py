70feae0aa07d448fc578d8345fa7d24930162dfd
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-20 12:16:21 +0200

    feat: add city_id and date_time unique together constraint on Temperature model, make migration files

--- temperature/models.py
+++ temperature/models.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
@@ -18,6 +18,11 @@ class Temperature(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     city_id: Mapped[int] = mapped_column(ForeignKey("cities.id", ondelete="CASCADE"))
     date_time: Mapped[datetime] = mapped_column()
-    temperature: Mapped[int] = mapped_column()
+    temperature: Mapped[float] = mapped_column()
 
     city: Mapped[City] = relationship(back_populates="temperatures")
+
+
+    __table_args__ = (
+        UniqueConstraint("city_id", "date_time", name="uq_city_date_time"),
+    )
